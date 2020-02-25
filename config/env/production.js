c55055b575544a582404d880c8f3919e48f14e90
ru03 <ruben.lmangas@gmail.com>
2020-02-25 20:40:21 +0100

    Add production to heroku

--- config/env/production.js
+++ config/env/production.js
@@ -1,12 +1,18 @@
 const production = {
   database: {
-    name: '',
-    password: '',
-    user: '',
+    name: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
   },
   logging: {
     deployment: 'PROD',
     level: 'verbose',
+  },
+  jwt: {
+    expiresIn: 3600,
+    secret: process.env.JWT_SECRET
   }
 }
 
